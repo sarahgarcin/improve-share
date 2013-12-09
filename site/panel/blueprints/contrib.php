189a34e91f598cee0d006c9259e2f09d507f7d54
sarah <sarahgarcin@hotmail.com>
2013-12-09 11:44:03 +0100

    Site ok présentation 3 décembre

---
+++ site/panel/blueprints/contrib.php
@@ -0,0 +1,21 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+# default blueprint
+
+title: Page
+pages: true
+files: true
+fields:
+  title: 
+    label: Titre
+    type:  text
+  author: 
+    label: Auteur
+    type:  text
+  year: 
+    label: Title
+    type:  text  
+  text: 
+    label: Text
+    type:  textarea
+    size:  large
--- site/templates/home.php
+++ site/templates/home.php
@@ -25,39 +25,50 @@
     <a href='#' id='close'></a>
 </div>
 
-<!--     <div class="intro">
-        <div class="close-buttons">X</div> 
-        <img src= "../../assets/images/traitpng2.png" class="trait" ></img>
-        <p>Introduction<br>
-        <br>
-         L'ambition d'Ivan Illich était immense : « (...) il faut inverser radicalement les institutions industrielles, reconstruire la société de fond en comble. »
-C'est un révolutionnaire et non un réformateur.
-Il est difficile de dire quel est l'héritage d'Illich mais il est probable que ce soit sa conception de l'outil qui reste la plus intéressante : « L’outil juste répond à trois exigences :
-il est générateur d'efficience sans dégrader l'autonomie personnelle,
-il ne suscite ni esclave ni maître,
-il élargit le rayon d'action personnelle».
-(Les externalités positives d'un outil conviviales sont supérieurs aux externalités négatives.)
-L'open source est un projet Illichien dont l'ambition est d'investir du temps, des connaissances pour créer des outils authentiques, par besoin ou par envie, et de rendre public ce travail. Les concepteurs  peuvent bénéficier des améliorations et des applications des autres utilisateurs. Les outils qui découlent de cette pratique sont des « outils conviviaux », justes.
-Dans le design graphique, la pratique du logiciel libre ne se limite pas, seulement, à l'utilisation de logicielsFLOSS (free/libre/open-source software) mais se traduit dans la conception des projets.
-Certains des travaux, que nous présentons, n'utilisent pas la programmation ou même l'informatique mais le process fait appel à certaines ambitions de l'open source sans pour autant en être le décalque ou l'illustration de ce qui ce fait dans la programmation. Ces travaux ne font pas appel à l'ensemble des caractéristiques du FLOSS (utiliser le logiciel sans restriction, étudier le logiciel, le modifier pour l’adapter à ses besoins et le redistribuer sous certaines conditions), mais : ils se créent en collaboration, débouchent sur la création d'outils spécifiques, partage les sources ou les outils... Il s'agit, pour nous, de projets, d'outils « conviviaux » qui contiennent en eux les caractéristiques de l'open source, de l'économie collaborative, tout comme la presse de Gutemberg contient en elle-même les caractéristiques de la modernité : reproduction de masse, travail à la chaîne.
-</p>
-    </div>  -->
-<!--     <div id='overlay'></div> -->
     <div class="intro-toggle">
-        <p class="intro-titre"> Enjeux et promesses des projets conviviaux </p>
-        <p class="contrib"><a href="<?php echo url('contrib') ?>" class="intro-text"> /////////  Fonctionnement et contributions  /////////</a></p>
-        <p class="intro-text">L'ambition d'Ivan Illich était immense : « (...) il faut inverser radicalement les institutions industrielles, reconstruire la société de fond en comble. »
-C'est un révolutionnaire et non un réformateur.
-Il est difficile de dire quel est l'héritage d'Illich mais il est probable que ce soit sa conception de l'outil qui reste la plus intéressante : « L’outil juste répond à trois exigences :
-il est générateur d'efficience sans dégrader l'autonomie personnelle,
-il ne suscite ni esclave ni maître,
-il élargit le rayon d'action personnelle».
-(Les externalités positives d'un outil conviviales sont supérieurs aux externalités négatives.)
-L'open source est un projet Illichien dont l'ambition est d'investir du temps, des connaissances pour créer des outils authentiques, par besoin ou par envie, et de rendre public ce travail. Les concepteurs  peuvent bénéficier des améliorations et des applications des autres utilisateurs. Les outils qui découlent de cette pratique sont des « outils conviviaux », justes.
-Dans le design graphique, la pratique du logiciel libre ne se limite pas, seulement, à l'utilisation de logicielsFLOSS (free/libre/open-source software) mais se traduit dans la conception des projets.
-Certains des travaux, que nous présentons, n'utilisent pas la programmation ou même l'informatique mais le process fait appel à certaines ambitions de l'open source sans pour autant en être le décalque ou l'illustration de ce qui ce fait dans la programmation. Ces travaux ne font pas appel à l'ensemble des caractéristiques du FLOSS (utiliser le logiciel sans restriction, étudier le logiciel, le modifier pour l’adapter à ses besoins et le redistribuer sous certaines conditions), mais : ils se créent en collaboration, débouchent sur la création d'outils spécifiques, partage les sources ou les outils... Il s'agit, pour nous, de projets, d'outils « conviviaux » qui contiennent en eux les caractéristiques de l'open source, de l'économie collaborative, tout comme la presse de Gutemberg contient en elle-même les caractéristiques de la modernité : reproduction de masse, travail à la chaîne.
-</p>
-    </div>
+        <p class="contrib"><a href="<?php echo url('contrib') ?>" > Enjeux et promesses</a></p>
+        <p class="intro-titre"> Fonctionnement et contribution </p>
+            <p class="intro-text">
+            Ce site est un état des lieux de travaux que nous appelons « conviviaux ». 
+            Il est la première étape pour notre projet de diplôme de l’EnsAD consacré à la pratique de l’open source dans le design graphique. 
+            Il présente des projets, des outils par un système de filtres:<br>
+            Création/augmentation d’outils, partage de la documentation, partage des sources, création en collaboration, 
+            workshop / conférences, utilisation de logiciels open-source, projets de recherche.
+            </p>
+            <p class="intro-text">
+            Le menu permet de sélectionner plusieurs filtres et ainsi croiser des catégories de projets. 
+            À vous de faire la combinaison de votre choix.
+            </p>
+            <p class="intro-text">
+            Ce site est un outil qui nous permet d’archiver des travaux que nous considérons comme des références. 
+            L’objectif, peut-être ambitieux, de notre projet de diplôme de L’EnsAD est de recouvrir l’ensemble des filtres. 
+            Comment présenter un site sur des projets contributifs et/ou conviviaux autrement que sous une forme contributive?<br>  
+            Nous serions ravis si vous souhaitiez contribuer, proposer un projet. Envoyez nous un email à l’adresse <strong>augmenter-partager@ensad.fr</strong>. 
+            Nous vous procurerons un identifiant.
+            </p>
+            <p class="intro-text">
+            Vous trouverez sur notre Titanpad l’ensemble des textes que nous avons rédigés (<a href="http://bigproject.titanpad.com/5" target="_blank"><strong>articles</strong></a>, <a href="http://bigproject.titanpad.com/6" target="_blank"><strong>enjeux et promesses</strong></a>). 
+            Une version de notre sera disponible sur Github afin de proposer des debugs, améliorations ou même utiliser le site pour vos propres projets.
+            </p>
+            <p class="intro-text">
+            Si le créateur de l’un des projets souhaite ne plus apparaître sur le site ou voudrait modifier l’article le concernant,    
+            il peut nous contacter à la même adresse mail. Nous rappelons que des droits d’auteurs s’appliquent sur certains des projets.
+            Nous n’en sommes pas les auteurs simplement le relai.
+            </p>
+            <p class="intro-text">
+            Nous remercions tout particulièrement : Jean-Baptiste Labrune, Francesca Cozzolino, Vadim Bernard, Ruedi Baur, Laurent Ungerer, 
+            Remy Bourganel ainsi que le service informatique de l’EnsAD.
+            </p>
+            <p class="intro-text">
+            Colophon :
+            Site réalisé grâce au CMS Kirby.
+            Typographie : Old standard TT
+            </p>
+            <p class="intro-text">
+            Cordialement,
+            Sarah Garcin, Victor Lebeau. Rédacteurs des articles et concepteurs du site.
+            </p>  
+        </div>
 
 	<div class="tags">
 
@@ -122,34 +133,7 @@ $(document).ready(function(){
 
 // Création d'une div de type "pays" + intégration des projets du pays dans la div
 
-    /* INTRODUCTION */
-
-    // var div = document.createElement('div');
-    // div.className = "introduction-wrapper"; 
-    // $('.projets').append(div);
-    // $('.introduction').each(function() {
-    //     $(div).append($(this));
-    // });
-
-
-    // $(".introduction-wrapper li").each(
-    //     function(intIndex) {
-
-    //         var l = Math.floor(Math.random() * $(".introduction-wrapper").width());
-    //         var t = Math.floor(Math.random() * $(".introduction-wrapper").height());
-
-    //     $(this).css("left", l);
-    //     $(this).css("top", t);
-
-    // }); 
-
-    //     $(document).on('click','.close-buttons',function(){
-    //     $('.intro').remove();
-    // });
-
-    //    $(document).on('click','.introduction',function(){
-    //       $('.introduction').remove();
-    // });
+ /* INTRO */
 
     $( ".intro-titre" ).click(function() {
         $( ".intro-text" ).toggle('slow');
